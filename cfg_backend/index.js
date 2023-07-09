9fa89ba436f8e422491adf0178bd2cb730f008d4
parakot-uncle <param.kothari@spit.ac.in>
2023-07-09 08:56:14 +0530

    final

--- cfg_backend/index.js
+++ cfg_backend/index.js
@@ -14,6 +14,14 @@ connectDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use("/api/student", studentRouter);
 app.use("/api/group", groupRouter);
 app.use("/api/objective", objectiveRouter);
